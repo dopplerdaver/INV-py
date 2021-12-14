8c43cb451f0d453f78cf4fba381b257e297c194a
Dave Serke <39059316+dopplerdaver@users.noreply.github.com>
2021-12-14 09:05:45 -0700

    Add files via upload

---
+++ Stock_strategy_plotter.py
@@ -0,0 +1,47 @@
+#------------------------------------------------------
+#
+# NAME: 	Stock_strategy_plotter.py
+#
+# PURPOSE:	strategy plotter
+#		1. Loading the feed from a CSV file.
+#		2. Running the strategy with the bars supplied by the feed and a StrategyPlotter attached.
+#		3. Plotting the strategy.
+#
+# CREATED:	11.12.2021 - dserke
+#
+# SOURCE:	http://gbeced.github.io/pyalgotrade/docs/v0.20/html/tutorial.html
+#
+#------------------------------------------------------
+
+#------------------------------------------------------
+# IMPORT LIBRARIES
+#------------------------------------------------------
+from pyalgotrade import plotter
+from pyalgotrade.barfeed import quandlfeed
+from pyalgotrade.stratanalyzer import returns
+import sma_crossover
+
+# Load the bar feed from the CSV file
+feed            = quandlfeed.Feed()
+feed.addBarsFromCSV("orcl", "WIKI-ORCL-2000-quandl.csv")
+
+# Evaluate the strategy with the feed's bars.
+myStrategy      = sma_crossover.SMACrossOver(feed, "orcl", 20)
+
+# Attach a returns analyzers to the strategy.
+returnsAnalyzer = returns.Returns()
+myStrategy.attachAnalyzer(returnsAnalyzer)
+
+# Attach the plotter to the strategy.
+plt             = plotter.StrategyPlotter(myStrategy)
+# Include the SMA in the instrument's subplot to get it displayed along with the closing prices.
+plt.getInstrumentSubplot("orcl").addDataSeries("SMA", myStrategy.getSMA())
+# Plot the simple returns on each bar.
+plt.getOrCreateSubplot("returns").addDataSeries("Simple returns", returnsAnalyzer.getReturns())
+
+# Run the strategy.
+myStrategy.run()
+myStrategy.info("Final portfolio value: $%.2f" % myStrategy.getResult())
+
+# Plot the strategy.
+plt.plot()
